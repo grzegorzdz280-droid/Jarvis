23de22e7f78dce5e26e8bcdc9aaea1f06e6dc326
grzegorzdz280-droid <grzegorzdz280@gmail.com>
2026-09-09 15:15:41 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 
 android { namespace = "com.jarvis.mobile"; compileSdk = 35
     defaultConfig { applicationId = "com.jarvis.mobile"; minSdk = 26; targetSdk = 35; versionCode = 3; versionName = "0.3.0" }
+    compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
+    kotlinOptions { jvmTarget = "17" }
 }
 
 dependencies {
